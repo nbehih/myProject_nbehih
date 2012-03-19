47910f674a0f734352155cad79d73cb07483bdaf
Nawal Behih <nabehih@gmail.com>
2012-03-19 17:25:30 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyProjectNbehih::Application.config.secret_token = '582dfbd885be54650f09d7683226619b0b47a2e9ac1364fb15d3d7292111ac120ec98dc82404e3bfefb127cdc1efe6bc9fe8a7dd4de0b0ea4ac0c1d5d1bd7948'
